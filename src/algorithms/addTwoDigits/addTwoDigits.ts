5d511037e917ebf0444c6ff91c04929473c60080
bartech-h <bartech.js@gmail.com>
2025-04-19 10:15:09 +0200

    progress

--- src/algorithms/addTwoDigits/addTwoDigits.ts
+++ src/algorithms/addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,6 @@
-function addTwoDigits(n: any): number {
-
+function addTwoDigits(n: number): number {
+  const numbers = n.toString().split("");
+  return parseInt(numbers[0]) + parseInt(numbers[1]);
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
